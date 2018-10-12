dec10d55e34bb8140353da0ccc1cf4a6f3c53de0
jgreenbaum <jgreenbaum@arbor.net>
2018-10-12 14:35:50 -0400

    AddMember component

---
+++ src/components/AddMember.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddMember = (props) => (
+    <form>
+        <input type='text' />
+        <input type='text' />
+        <button>Add Member</button>
+    </form>
+);
+
+export default AddMember;
--- src/components/PartyList.js
+++ src/components/PartyList.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import AddMember from './AddMember';
 import PartyMember from './PartyMember';
 
 const PartyList = ({party}) => (
     <div>
         <h3>Party Members</h3>
-        <form>
-            {party.map((member) => {
-                return <PartyMember member={member} />
-            })}
-        </form>
+        {party.map((member) => {
+            return <PartyMember member={member} />
+        })}
+        <AddMember />
     </div>
 );
 
