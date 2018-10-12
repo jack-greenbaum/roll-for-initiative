bf2dde6c716a144f07f43fae1af699531f2ca88e
jgreenbaum <jgreenbaum@arbor.net>
2018-10-12 09:13:46 -0400

    Add PartyInput component and class properties Babel plugin

--- src/components/InitiativeApp.js
+++ src/components/InitiativeApp.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './Header';
+import PartyInput from './PartyInput';
 
 export default class InitiativeApp extends React.Component {
+    state = {
+        party: []
+    }
     render() {
         return (
             <div>
                 <Header title="Roll for Initiative!"/>
+                <PartyInput />
             </div>
         );
     }
---
+++ src/components/PartyInput.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PartyInput = () => (
+    <div>This is my party input</div>
+);
+
+export default PartyInput;
